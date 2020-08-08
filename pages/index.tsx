aa3d283513e0d5e8239042bdbfa3442a4b9bf767
Meghana Bhimasani <mbhimasani3@gatech.edu>
2020-08-08 15:09:04 -0400

    added anchor uitl

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,15 @@
 import React from "react";
 import { motion } from "framer-motion";
 import { fadeInPage } from "../animations/page";
+import Anchor from "../components/utils/anchor";
 
 const Home: React.FC = () => (
   <motion.main className="body" {...fadeInPage}>
-    <h2 className="begin-section pt-0">I'm Ash Bhimasani</h2>
+    <h2 className="begin-section pt-0">I'm Meghana Bhimasani</h2>
     <div className="lead end-section">
-      I am a product designer & software engineer with special interests in UX,
-      growth hacking, and business strategy currently working
+      I am a software engineer with a background in healthcare and UX and special interests in
+      machine learning, interactive intelligence, and data-driven decision making currently working
+      <Anchor content="@NeurophotonicsLab" href="https://sites.wustl.edu/eggebrechtlab/" spaceBefore />.
     </div>
 
   </motion.main>
